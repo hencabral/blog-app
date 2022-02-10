f1088ebc3245d5232774d619dcaab05d42aa099c
xzanella <lhenriquezan@gmail.com>
2022-02-10 20:45:48 -0300

    feat: add admin/index page

--- routes/admin.js
+++ routes/admin.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.send('admin/index');
+    res.render('admin/index');
 });
 
 module.exports = router;

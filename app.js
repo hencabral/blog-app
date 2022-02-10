f3be4ee4f7f47d0861f05aedb32fb01319f6930c
xzanella <lhenriquezan@gmail.com>
2022-02-10 20:58:24 -0300

    feat: add public bootstrap

--- app.js
+++ app.js
@@ -4,11 +4,15 @@ const express = require('express');
 const handlebars = require('express-handlebars');
 const app = express();
 const admin = require('./routes/admin');
+const path = require('path');
 
 const PORT = 8081;
 
 app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+
+//Public
+app.use(express.static(path.join(__dirname, "public")));
 
 //Rotas
 app.use('/admin', admin);

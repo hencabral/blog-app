82e9b410c1733e5bef3ab7ba66040e3c8ad21ae5
Luiz Henrique Cabral <42081493+xZANELLA@users.noreply.github.com>
2022-02-10 17:18:37 -0300

    chore: cofigure handlebars defaultLayout 'main'

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 //Carregando módulos
 
 const express = require('express');
+const handlebars = require('express-handlebars');
 const app = express();
 
 const PORT = 8081;
+
+app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+//Rotas
+app.get("/", (req, res) => {
+    res.send("Rota/");
+})
 
 app.listen(PORT, () => {
     console.log("Servidor rodando na porta: " + PORT);

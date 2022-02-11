f1e6a36e77effa94036ab367d8d061b4c2158618
Luiz Henrique Cabral <42081493+xZANELLA@users.noreply.github.com>
2022-02-11 16:05:34 -0300

    feat: add admin/categorias/edit post route
    validates categoria edit form and saves edited data in Mongo

--- routes/admin.js
+++ routes/admin.js
@@ -71,8 +71,58 @@ router.get("/categorias/edit/:id", (req, res) => {
     })
     .catch((err) => {
         req.flash("error_msg", "Categoria não localizada");
-        res.redirect("admin/categorias");
+        res.redirect("/admin/categorias");
     });
 });
+
+router.post("/categorias/edit", (req, res) => {
+    
+    Categoria.findOne({_id: req.body.id})
+    .then((categoria) => {
+
+        //Validando formulario edição de categoria
+        var erros = [];
+
+        if(!req.body.nome || typeof req.body.nome == undefined || req.body.nome == null){
+            erros.push({texto: "Nome inválido"});
+        }
+
+        if(!req.body.slug || typeof req.body.slug == undefined || req.body.slug == null){
+            erros.push({texto: "Slug inválido"});
+        }
+
+        if(req.body.nome.length < 2){
+            erros.push({texto: "Nome da categoria muito pequeno"});
+        }
+
+        if(erros.length > 0){
+            Categoria.findOne({ _id: req.body.id }).lean().then((categoria) => {
+                res.render("admin/editcategorias", { categoria: categoria, erros: erros})
+            }).catch((err) => {
+                req.flash("error_msg", "Erro ao pegar os dados")
+                res.redirect("admin/categorias")
+            })
+        }else{
+
+            categoria.nome = req.body.nome;
+            categoria.slug = req.body.slug;
+
+            categoria.save()
+            .then(() => {
+                req.flash("success_msg", "Categoria editada com sucesso!");
+                res.redirect("/admin/categorias");
+            })
+            .catch((err) => {
+                req.flash("error_msg", "Erro ao editar categoria");
+                res.redirect("/admin/categorias");
+            });
+         }
+    })
+    .catch((err) => {
+        console.log(err);
+        req.flash("error_msg", "Erro ao editar a categoriaa");
+        res.redirect("/admin/categorias");
+    })
+})
 
 module.exports = router;

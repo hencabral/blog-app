feb5696f09b50cbc51cd30e8422812832a3956b3
xzanella <lhenriquezan@gmail.com>
2022-02-10 21:55:37 -0300

    chore: cofigure mongoose

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const admin = require('./routes/admin');
 const path = require('path');
 const session = require('express-session');
 const flash = require('connect-flash');
+const mongoose = require('mongoose');
 
 const PORT = 8081;
 
@@ -25,8 +26,19 @@ app.use((req, res, next) => {
     next();
 });
 
+//Handlebars
 app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+
+//Mongoose
+const uri = "mongodb+srv://root:root@test.m7oxe.mongodb.net/blogApp?retryWrites=true&w=majority";
+mongoose.Promise = global.Promise;
+mongoose.connect(uri)
+.then(() => {
+    console.log("mongoDB Conectado....")
+}).catch((err) => {
+    console.log("Houve um erro ao se conectar ao mongoBD: " + err)
+});
 
 //Public
 app.use(express.static(path.join(__dirname, "public")));

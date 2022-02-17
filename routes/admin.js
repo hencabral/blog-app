3f8a8a7579e8307aa7fbc76db6e49b4d47e56ec9
Luiz Henrique Cabral <42081493+xZANELLA@users.noreply.github.com>
2022-02-17 17:11:25 -0300

    feat: admin/postagens search posts
    fetch postagens from the db render admin/postagens passing the postagens

--- routes/admin.js
+++ routes/admin.js
@@ -142,7 +142,15 @@ router.get("/categorias/deletar/:id", (req, res) => {
 });
 //Rota para exibir postagens
 router.get("/postagens", (req, res) => {
-    res.render("admin/postagens");
+
+    Postagem.find().lean().populate("categoria").sort({data: "desc"})
+    .then((postagens) => {
+        res.render("admin/postagens", {postagens: postagens});
+    })
+    .catch((err) => {
+        req.flash("error_msg", "Houve um erro ao listar as postagens");
+        res.redirect("/admin");
+    });
 });
 
 router.get("/postagens/add", (req, res) => {

88f039b36fb5b18eb6bea3e4f1111b7fb9c83155
Luiz Henrique Cabral <42081493+xZANELLA@users.noreply.github.com>
2022-02-18 17:31:15 -0300

    feat: add admin/postagens/edit route
    fetch postagens by id, categorias and render page admin/editcategorias

--- routes/admin.js
+++ routes/admin.js
@@ -215,4 +215,24 @@ router.post("/postagens/nova", (req, res) => {
     }
 });
 
+router.get("/postagens/edit/:id", (req, res) =>{
+    Postagem.findOne({_id: req.params.id}).lean()
+    .then((postagem) => {
+
+        Categoria.find().lean()
+        .then((categorias) => {
+
+            res.render("admin/editpostagens", {categorias: categorias, postagem: postagem});
+        })
+        .catch((err) => {
+            req.flash("error_msg", "Categoria não localizada");
+            res.redirect("/admin/postagens");
+        });
+    })
+    .catch((err) => {
+        req.flash("error_msg", "Postagem não localizada");
+        res.redirect("/admin/postagens");
+    });
+});
+
 module.exports = router;

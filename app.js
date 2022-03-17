26d0dcaafcc540097256f8aaa04756b80a62b670
xzanella <lhenriquezan@gmail.com>
2022-03-16 22:02:40 -0300

    feat: add /categorias route

--- app.js
+++ app.js
@@ -10,7 +10,9 @@ const session = require('express-session');
 const flash = require('connect-flash');
 const mongoose = require('mongoose');
 require("./models/Postagem");
-const Postagem = mongoose.model("postagens")
+require("./models/Categoria");
+const Postagem = mongoose.model("postagens");
+const Categoria = mongoose.model("categorias");
 
 const PORT = 8081;
 
@@ -72,6 +74,17 @@ app.get('/postagem/:slug', (req, res) => {
     .catch((err) => {
         req.flash("error_msg", "Erro ao exibir postagem");
         res.redirect("/404");
+    });
+});
+
+app.get('/categorias', (req, res) => {
+    Categoria.find().lean()
+    .then((categorias) => {
+        res.render('categorias/index', {categorias: categorias});
+    })
+    .catch((err) => {
+        req.flash("error_msg", "Houve um erro ao listar as categorias");
+        res.redirect('/');
     });
 });
 

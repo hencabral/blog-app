e708da9000969802df05857add679eb4579b29b7
Luiz Henrique Cabral <42081493+xZANELLA@users.noreply.github.com>
2022-03-21 10:04:23 -0300

    feat: add route post usuarios/registro

--- routes/usuario.js
+++ routes/usuario.js
@@ -9,4 +9,72 @@ router.get('/registro', (req, res) => {
     res.render('usuarios/registro');
 });
 
+router.post('/registro', (req, res) => {
+    var erros = [];
+
+    if(!req.body.nome || typeof req.body.nome == undefined || req.body.nome == null ){
+        erros.push({texto: "Nome inválido"});
+    }
+
+    if(!req.body.email || typeof req.body.email == undefined || req.body.email == null ){
+        erros.push({texto: "E-mail inválido"});
+    }
+
+    if(!req.body.senha || typeof req.body.senha == undefined || req.body.senha == null ){
+        erros.push({texto: "Senha inválida"});
+    }
+
+    if(req.body.senha.length < 4){
+        erros.push({texto: "Senha muito curta!"});
+    }
+
+    if(req.body.senha != req.body.senha2){
+        erros.push({texto: "As senha não são iguais!"});
+    }
+
+    if(erros.length > 0){
+        res.render('usuarios/registro', {erros});
+
+    }else{
+        Usuario.findOne({email: req.body.email}).lean()
+        .then((usuario) => {
+            if(usuario){
+                req.flash("error_msg", "E-mail já cadastrado");
+                res.redirect("/usuarios/registro");
+            }else{
+                const novoUsuario = new Usuario({
+                    nome: req.body.nome,
+                    email: req.body.email,
+                    senha: req.body.senha
+                });
+
+                bcrypt.genSalt(10, (erro, salt) => {
+                    bcrypt.hash(novoUsuario.senha, salt, (erro, hash) => {
+                        if(erro){
+                            req.flash("error_msg", "Erro ao cadastrar usuário");
+                            res.redirect("/usuarios/registro");
+                        }
+
+                        novoUsuario.senha = hash;
+
+                        novoUsuario.save()
+                        .then(() => {
+                            req.flash("success_msg", "Usuário cadastrado com sucesso!");
+                            res.redirect("/");
+                        })
+                        .catch((err) => {
+                            req.flash("error_msg", "Erro ao cadastrar usuário");
+                            res.redirect("/usuarios/registro");
+                        });
+                    });
+                });
+            }
+        })
+        .catch((err) => {
+            req.flash("error_msg", "Houve um erro ao verificar e-mail");
+            res.redirect("/");
+        });
+    }
+});
+
 module.exports = router
